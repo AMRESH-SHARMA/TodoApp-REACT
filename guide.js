c021ec65da76b6e627ac2c9ae95fae7055ca0a76
AMRESH <amreshaiio@gmail.com>
2022-07-28 20:59:24 +0530

    Add and del noteitems

---
+++ guide.js
@@ -0,0 +1,6 @@
+// Create:
+// New React app
+// Header component, header.jsx to show keeper app name in h1 tag
+// Footer component, footer.jsx to show copyright msg, dynamically fetched year.
+
+// Note component to add notes in our app.
